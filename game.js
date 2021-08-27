4037770ab48b2051804c54579ac0de653736550d
Nycole <nycole.xavierr@gmail.com>
2021-08-27 10:38:02 -0300

    add chao and background

--- game.js
+++ game.js
@@ -6,6 +6,50 @@ sprites.src = 'assets/cenario/sprites.png';
 const canvas = document.querySelector('canvas')
 const contexto = canvas.getContext('2d')
 
+// background 
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: cvs.height - 204,
+
+    desenha() {
+        ctx.fillStyle = '#70c5ce';
+        ctx.fillStyle (0, 0, cvs.width, cvs.height);
+
+        ctx.drawImage(
+            sprite,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
+    },
+};
+
+// Chão
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    // altura total do canva e subtrai 112
+    y: canvas.height - 112,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
+    },
+
+};
+
 // nosso personagem
 const flappyBird = {
     spriteX: 0,
@@ -28,6 +72,7 @@ const flappyBird = {
 
 // ajudar a reproduzir os quadros do jogo
 function loop () {
+    // chama a função que esta dentro da variável fllapyBird
     flappyBird.desenha();
 
     // ajudar a reproduzir os quadros do jogo
